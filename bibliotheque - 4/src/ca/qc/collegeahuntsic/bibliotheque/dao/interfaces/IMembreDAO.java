f3800278c952ae2c70293a325f1f1b750e657225
vincentblais <vincent.blais.2@collegeahuntsic.qc.ca>
2014-10-03 15:18:25 -0400

    IPretDAO et IMembreDAO

---
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
@@ -0,0 +1,17 @@
+// Fichier IMembreDAO.java
+// Auteur : Vincent
+// Date de création : Oct 3, 2014
+
+package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
+
+/**
+ * TODO Auto-generated class javadoc
+ *
+ * @author Vincent
+ */
+public interface IMembreDAO extends IDAO {
+    /*
+     * interface vide
+     */
+
+}
