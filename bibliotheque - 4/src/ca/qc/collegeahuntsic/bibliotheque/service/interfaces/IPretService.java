8b9ae4cd0a1578e78c3356c80b85e9c8be8c3710
Samuel Filion-Normandeau <samuelfilion@hotmail.com>
2014-10-03 12:40:01 -0400

    + créé (et configuré l'héritage)... 	+ IMembreService 	+ IPretService

---
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/service/interfaces/IPretService.java
@@ -0,0 +1,9 @@
+// Fichier Facade.java
+// Auteur : Samuel Filion-Normandeau
+// Date de création : 2014-10-03
+
+package ca.qc.collegeahuntsic.bibliotheque.service.interfaces;
+
+public interface IPretService extends IService{
+
+}
