68aba42ec19d504613487266f1ba2ad355b9e9b8
Chou Huynh <Chou.Huynh.1@collegeahuntsic.qc.ca>
2014-10-03 16:20:47 -0400

    Version avec membre service updater mais pas complet

--- bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/service/interfaces/IMembreService.java
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/service/interfaces/IMembreService.java
@@ -4,36 +4,97 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.service.interfaces;
 
+import java.util.List;
 import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
-import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyRequestException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ServiceException;
 
-public interface IMembreService extends IService{
+public interface IMembreService extends IService {
 
-	/**
-     * Ajoute un nouveau membre dans la base de données.
+    /**
+     * Ajoute un nouveau livre dans la base de données.
      * 
      * @param connexion La connexion à utiliser
-     * @param membreDTO Le membre à ajouter
+     * @param livreDTO Le livre à ajouter
      * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le membre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du membre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyRequestException Si la requête de la clef primaire du membre est <code>null</code>
+     * @throws InvalidDTOException Si le livre est <code>null</code>
+     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
+     * @throws InvalidPrimaryKeyRequestException Si la requête de la clef primaire du livre est <code>null</code>
      * @throws ServiceException S'il y a une erreur avec la base de données
      */
-	void add(Connexion connexion, MembreDTO membreDTO)
-			throws InvalidHibernateSessionException, InvalidDTOException,
-			InvalidDTOClassException, InvalidPrimaryKeyRequestException,
-			ServiceException;
+    void add(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        InvalidPrimaryKeyRequestException,
+        ServiceException;
 
-	MembreDTO get(Connexion connexion, String idMembre)
-			throws InvalidHibernateSessionException,
-			InvalidPrimaryKeyException, ServiceException;
+    /**
+     * Lit un livre à partir de la base de données.
+     * 
+     * @param connexion La connexion à utiliser
+     * @param idLivre L'ID du livre à lire
+     * @return Le livre
+     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
+     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
+     * @throws ServiceException S'il y a une erreur avec la base de données
+     */
+    MembreDTO get(Connexion connexion,
+        String idMembre) throws InvalidHibernateSessionException,
+        InvalidPrimaryKeyException,
+        ServiceException;
 
+    /**
+     * Met à jour un livre dans la base de données.
+     * 
+     * @param connexion La connexion à utiliser
+     * @param livreDTO Le livre à mettre à jour
+     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
+     * @throws InvalidDTOException Si le livre est <code>null</code>
+     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
+     * @throws ServiceException S'il y a une erreur avec la base de données
+     */
+    void update(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        ServiceException;
+
+    /**
+     * Supprime un livre de la base de données.
+     * 
+     * @param connexion La connexion à utiliser
+     * @param livreDTO Le livre à supprimer
+     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
+     * @throws InvalidDTOException Si le livre est <code>null</code>
+     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
+     * @throws ServiceException S'il y a une erreur avec la base de données
+     */
+    void delete(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        ServiceException;
+
+    /**
+     * Trouve tous les livres de la base de données. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun
+     * livre n'est trouvé, une {@link List} vide est retournée.
+     * 
+     * @param connexion La connexion à utiliser
+     * @param sortByPropertyName The nom de la propriété à utiliser pour classer
+     * @return La liste de tous les livres ; une liste vide sinon
+     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
+     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
+     * @throws ServiceException S'il y a une erreur avec la base de données
+     */
+    List<MembreDTO> getAll(Connexion connexion,
+        String sortByPropertyName) throws InvalidHibernateSessionException,
+        InvalidSortByPropertyException,
+        ServiceException;
 }
