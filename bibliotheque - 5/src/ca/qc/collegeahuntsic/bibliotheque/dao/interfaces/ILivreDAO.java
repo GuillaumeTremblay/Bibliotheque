8596a723fa2ec0a9d5138b46e8336b758b190e5f
jonathan aumont <jonathan.aumont.1@collegeahuntsic.qc.ca>
2014-10-15 12:47:37 -0400

    java doc

--- bibliotheque - 5/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/ILivreDAO.java
+++ bibliotheque - 5/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/ILivreDAO.java
@@ -5,12 +5,12 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
 
 import java.util.List;
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import org.hibernate.Session;
 
 /**
  * Interface DAO pour manipuler les livres dans la base de données.
@@ -22,7 +22,7 @@ public interface ILivreDAO extends IDAO {
      * Trouve les livres à partir d'un titre. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun livre
      * n'est trouvé, une {@link List} vide est retournée.
      * 
-     * @param connexion La connexion à utiliser
+     * @param session La session Hibernate à utiliser
      * @param titre Le titre à trouver
      * @param sortByPropertyName The nom de la propriété à utiliser pour classer
      * @return La liste des livres correspondants ; une liste vide sinon
@@ -31,7 +31,7 @@ public interface ILivreDAO extends IDAO {
      * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
      * @throws DAOException S'il y a une erreur avec la base de données
      */
-    List<LivreDTO> findByTitre(Connexion connexion,
+    List<LivreDTO> findByTitre(Session session,
         String titre,
         String sortByPropertyName) throws InvalidHibernateSessionException,
         InvalidCriterionException,
