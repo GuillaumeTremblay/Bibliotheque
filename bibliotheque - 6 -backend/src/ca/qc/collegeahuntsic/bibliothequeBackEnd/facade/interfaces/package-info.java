a94b9b147b661f5dcc02e5802c340ebc7905b753
Chou Huynh <Chou.Huynh.1@collegeahuntsic.qc.ca>
2014-11-04 13:13:16 -0500

    Version avec toute ce qui est modifier. (pt2)

---
+++ bibliotheque - 6 -backend/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/facade/interfaces/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : 201205090
+// Date de création : 2014-10-30
+/**
+ * Package des interfaces des facades.
+ *
+ * @author Chou Huynh
+ */
+
+package ca.qc.collegeahuntsic.bibliothequeBackEnd.facade.interfaces;
+
---
+++ bibliotheque - 6 -backend/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/util/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : 201205090
+// Date de création : 2014-10-30
+/**
+ * Package des utilités.
+ *
+ * @author Chou Huynh
+ */
+
+package ca.qc.collegeahuntsic.bibliothequeBackEnd.util;
+
