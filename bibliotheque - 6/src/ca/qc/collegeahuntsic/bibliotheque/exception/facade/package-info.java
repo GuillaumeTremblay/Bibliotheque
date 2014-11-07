15e276f3653c172b2e66d75a8699854e9da4ed20
Vincent Blais <vincent.blais.2@collegeahuntsic.qc.ca>
2014-11-07 15:16:14 -0500

    Revert "packageinfo frontend"
    This reverts commit a32be9258bf0427255885e613dacfa83328ff75e.

--- bibliotheque - 6/src/ca/qc/collegeahuntsic/bibliotheque/exception/facade/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Ce package contient les exceptions facade.
- * 
- * @author 200825393
- *
- */
-
-package ca.qc.collegeahuntsic.bibliotheque.exception.facade;
-
--- bibliotheque - 6/src/ca/qc/collegeahuntsic/bibliotheque/exception/service/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Ce package contient les exceptions service.
- * 
- * @author 200825393
- *
- */
-
-package ca.qc.collegeahuntsic.bibliotheque.exception.service;
-
