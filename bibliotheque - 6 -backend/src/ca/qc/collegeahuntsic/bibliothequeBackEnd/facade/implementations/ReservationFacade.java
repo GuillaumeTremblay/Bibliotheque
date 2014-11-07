c8ee522da3cbbdd81d9f520f7c898a447382aa38
Guillaume Tremblay <guillaume.tremblay4@collegeahuntsic.qc.ca>
2014-11-07 14:43:38 -0500

    Version avec javadoc

--- bibliotheque - 6 -backend/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/facade/implementations/ReservationFacade.java
+++ bibliotheque - 6 -backend/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/facade/implementations/ReservationFacade.java
@@ -21,7 +21,6 @@ import ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.service.MissingLoanEx
 import ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.service.ServiceException;
 import ca.qc.collegeahuntsic.bibliothequeBackEnd.facade.interfaces.IReservationFacade;
 import ca.qc.collegeahuntsic.bibliothequeBackEnd.service.interfaces.IReservationService;
-
 import org.hibernate.Session;
 
 /**
@@ -30,112 +29,127 @@ import org.hibernate.Session;
  * @author Gilles Benichou
  */
 public class ReservationFacade extends Facade implements IReservationFacade {
-	private IReservationService reservationService;
+    private IReservationService reservationService;
 
-	/**
-	 * Crée la façade de la table <code>reservation</code>.
-	 *
-	 * @param reservationService
-	 *            Le service de la table <code>reservation</code>
-	 * @throws InvalidServiceException
-	 *             Si le service de réservations est <code>null</code>
-	 */
-	ReservationFacade(IReservationService reservationService)
-			throws InvalidServiceException {
-		super();
-		if (reservationService == null) {
-			throw new InvalidServiceException(
-					"Le service de réservations ne peut être null");
-		}
-		setReservationService(reservationService);
-	}
+    /**
+     * Crée la façade de la table <code>reservation</code>.
+     *
+     * @param reservationService
+     *            Le service de la table <code>reservation</code>
+     * @throws InvalidServiceException
+     *             Si le service de réservations est <code>null</code>
+     */
+    ReservationFacade(IReservationService reservationService) throws InvalidServiceException {
+        super();
+        if(reservationService == null) {
+            throw new InvalidServiceException("Le service de réservations ne peut être null");
+        }
+        setReservationService(reservationService);
+    }
 
-	// Region Getters and Setters
-	/**
-	 * Getter de la variable d'instance <code>this.reservationService</code>.
-	 *
-	 * @return La variable d'instance <code>this.reservationService</code>
-	 */
-	private IReservationService getReservationService() {
-		return this.reservationService;
-	}
+    // Region Getters and Setters
+    /**
+     * Getter de la variable d'instance <code>this.reservationService</code>.
+     *
+     * @return La variable d'instance <code>this.reservationService</code>
+     */
+    private IReservationService getReservationService() {
+        return this.reservationService;
+    }
 
-	/**
-	 * Setter de la variable d'instance <code>this.reservationService</code>.
-	 *
-	 * @param reservationService
-	 *            La valeur à utiliser pour la variable d'instance
-	 *            <code>this.reservationService</code>
-	 */
-	private void setReservationService(IReservationService reservationService) {
-		this.reservationService = reservationService;
-	}
+    /**
+     * Setter de la variable d'instance <code>this.reservationService</code>.
+     *
+     * @param reservationService
+     *            La valeur à utiliser pour la variable d'instance
+     *            <code>this.reservationService</code>
+     */
+    private void setReservationService(IReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
-	// EndRegion Getters and Setters
+    // EndRegion Getters and Setters
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void placer(Session session, ReservationDTO reservationDTO)
-			throws InvalidHibernateSessionException, InvalidDTOException,
-			InvalidPrimaryKeyException, MissingDTOException,
-			InvalidCriterionException, InvalidSortByPropertyException,
-			MissingLoanException, ExistingLoanException,
-			ExistingReservationException, InvalidDTOClassException,
-			FacadeException {
-		try {
-			getReservationService().placer(session, reservationDTO);
-		} catch (ServiceException serviceException) {
-			throw new FacadeException(serviceException);
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void placer(Session session,
+        ReservationDTO reservationDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        MissingLoanException,
+        ExistingLoanException,
+        ExistingReservationException,
+        InvalidDTOClassException,
+        FacadeException {
+        try {
+            getReservationService().placer(session,
+                reservationDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void utiliser(Session session, ReservationDTO reservationDTO)
-			throws InvalidHibernateSessionException, InvalidDTOException,
-			InvalidPrimaryKeyException, MissingDTOException,
-			InvalidCriterionException, InvalidSortByPropertyException,
-			ExistingReservationException, ExistingLoanException,
-			InvalidLoanLimitException, InvalidDTOClassException,
-			FacadeException {
-		try {
-			getReservationService().utiliser(session, reservationDTO);
-		} catch (ServiceException serviceException) {
-			throw new FacadeException(serviceException);
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void utiliser(Session session,
+        ReservationDTO reservationDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        ExistingReservationException,
+        ExistingLoanException,
+        InvalidLoanLimitException,
+        InvalidDTOClassException,
+        FacadeException {
+        try {
+            getReservationService().utiliser(session,
+                reservationDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void annuler(Session session, ReservationDTO reservationDTO)
-			throws InvalidHibernateSessionException, InvalidDTOException,
-			InvalidPrimaryKeyException, MissingDTOException,
-			InvalidDTOClassException, FacadeException {
-		try {
-			getReservationService().annuler(session, reservationDTO);
-		} catch (ServiceException serviceException) {
-			throw new FacadeException(serviceException);
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void annuler(Session session,
+        ReservationDTO reservationDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidDTOClassException,
+        FacadeException {
+        try {
+            getReservationService().annuler(session,
+                reservationDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public ReservationDTO getReservation(Session session, String idReservation)
-			throws InvalidHibernateSessionException,
-			InvalidPrimaryKeyException, FacadeException {
-		try {
-			return getReservationService().getReservation(session,
-					idReservation);
-		} catch (ServiceException serviceException) {
-			throw new FacadeException(serviceException);
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ReservationDTO getReservation(Session session,
+        String idReservation) throws InvalidHibernateSessionException,
+        InvalidPrimaryKeyException,
+        FacadeException {
+        try {
+            return getReservationService().getReservation(session,
+                idReservation);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+    }
 }
